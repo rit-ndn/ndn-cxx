e2e7a849f523fbe57bdcef1b94820c01b1b0a8ff
Carlos Barrios <cabeee@rit.edu>
2024-11-08 00:20:45 -0500

    fixed missing rpi5 MACs.

--- run_scripts/dag_run_local.sh
+++ run_scripts/dag_run_local.sh
@@ -52,8 +52,8 @@ rpi5rtr2ETHMAC=2c:cf:67:4d:af:4a
 rpi5rtr2WiFiMAC=2c:cf:67:4d:af:4b
 rpi5rtr3ETHMAC=2c:cf:67:4d:af:02
 rpi5rtr3WiFiMAC=2c:cf:67:4d:af:03
-rpi5producerETHMAC=NULL
-rpi5producerWiFiMAC=NULL
+rpi5producerETHMAC=2c:cf:67:6d:e3:69
+rpi5producerWiFiMAC=2c:cf:67:6d:e3:6c
 
 lenovoETHinterface=eno1
 jetsonETHinterface=eth0
